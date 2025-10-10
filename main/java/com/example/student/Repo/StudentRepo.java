ba8a71abd14e23d8a66563093aac979540c07468
innobadal98 <badal.patel@innogent.in>
2025-10-10 13:40:52 +0530

    Delete main directory

--- main/java/com/example/student/Repo/StudentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.student.Repo;
-
-import com.example.student.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student, Long> {
-}
