4e6f4efc3f4f502d1a2a218f3ab31033835097de
innobadal98 <badal.patel@innogent.in>
2025-10-10 13:38:42 +0530

    spring boot assignment

---
+++ main/java/com/example/student/entity/Student.java
@@ -0,0 +1,55 @@
+package com.example.student.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+
+    public Student(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
